c41f76ac099012974474d3cb27cd251727ea528b
Matt Blenkinsop <mgblenkinsop@outlook.com>
2022-10-03 16:22:51 +0100

    guest access added, parallax deleted

--- components/projects/ProjectDetail.js
+++ components/projects/ProjectDetail.js
@@ -1,8 +1,37 @@
 import React, { useState } from 'react'
-import GuestAccessModal from './GuestAccessModal'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+
 
 export default function ProjectDetail({ data, index }) {
-  const [guestAccessModal, setGuestAccessModal] = useState(false)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+  const url = process.env.NEXT_PUBLIC_GUEST_ACCESS_CREATE
+  const router = useRouter()
+
+  const handleClick = async () => {
+
+    setIsPending(true)
+    try {
+        const res = await axios(url,
+        { 
+            withCredentials: true, 
+            credentials: 'include' 
+        })
+        if(res.data.token){
+          const url = `https://solschfantasydraft.co.uk/login?token=${res.data.token}`
+          router.push(url)
+        }
+
+        // setAccessSent(true)
+        // setIsPending(false)
+    } catch(err) {
+        console.log(err)
+        setIsPending(false)
+    }
+    
+}
 
   return (
     <>
@@ -10,7 +39,8 @@ export default function ProjectDetail({ data, index }) {
         <h3 className=''>{data.useage}</h3>
         { !data.host && data.title !== 'Portfolio Page' && <a href={data.url} target="_blank" rel="noreferrer" className="general-btn-sm md:general-btn" disabled>This project is not currently being hosted</a>}
         { data.host && !data.guest_access && <a href={data.url} target="_blank" rel="noreferrer" className="general-btn-sm md:general-btn">Click here for a live version hosted with {data.host}</a>}
-        { data.host && data.guest_access && <button className='general-btn-sm md:general-btn' onClick={() => setGuestAccessModal(true)}>Click here for a live version hosted with {data.host}</button>}
+        { data.host && data.guest_access && !isPending && <button className='general-btn-sm md:general-btn' onClick={handleClick}>Click here for a live version hosted with {data.host}</button>}
+        { data.host && data.guest_access && isPending && <button className='general-btn-sm md:general-btn' disabled>Creating access token...</button>}
         <h4 className='text-lg lg:text-xl underline font-semibold'>Skills learned</h4>
         <p className=''>{data.learned}</p>
         <h4 className='text-lg lg:text-xl underline font-semibold'>Background</h4>
@@ -18,7 +48,6 @@ export default function ProjectDetail({ data, index }) {
         <h4 className='text-lg lg:text-xl underline font-semibold'>Challenges faced</h4>
         <p className=''>{data.challenges}</p>
         <br />
-        { guestAccessModal && <GuestAccessModal setGuestAccessModal={setGuestAccessModal} />}
     </>
   )
 }
