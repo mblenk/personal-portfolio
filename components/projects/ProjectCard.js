18c1e9082e53a88feea23be68ca8f304b29eafdd
Matt Blenkinsop <mgblenkinsop@outlook.com>
2022-10-03 16:44:49 +0100

    pointer cursor added

--- components/projects/ProjectCard.js
+++ components/projects/ProjectCard.js
@@ -5,7 +5,7 @@ export default function ProjectCard({ image, setActiveSlide, index }) {
 
   return (
         <div className="lg:h-96 w-full bg-secondary rounded-lg">
-            <img src={image} alt="Golf Handicap Calculator webpage image" className='w-full h-fullrounded-lg object-cover' onClick={() => setActiveSlide(index)} />
+            <img src={image} alt="Golf Handicap Calculator webpage image" className='w-full h-fullrounded-lg object-cover cursor-pointer' onClick={() => setActiveSlide(index)} />
         </div>
   )
 }
